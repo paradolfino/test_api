4188b35571850d0d466e0953c446e1fb5d77a642
viktharien <viktharien@zoho.com>
2018-03-29 20:30:49 -0500

    create before_action and private methods for controllers

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,29 @@
+class EntriesController < ApplicationController
+  before_action :set_entry, only: [:show, :update, :destroy]
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  private
+
+    def params_entry
+      params.permit(:content, :declaration_id)
+    end
+end
