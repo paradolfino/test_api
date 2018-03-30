03606b86f36dc4db504b0b9559d05dbb96f3a37e
viktharien <viktharien@zoho.com>
2018-03-29 20:25:37 -0500

    create test controllers, models, actions, routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180330011902) do
+ActiveRecord::Schema.define(version: 20180330012506) do
 
   create_table "declarations", force: :cascade do |t|
     t.string "content"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20180330011902) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "declaration_id"
+    t.index ["declaration_id"], name: "index_entries_on_declaration_id"
   end
 
 end
