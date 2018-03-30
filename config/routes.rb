9a8b8281eefe5a134c48fdc531b09db8522bc36e
viktharien <viktharien@zoho.com>
2018-03-29 20:22:59 -0500

    create test controllers, models, actions, routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,4 @@
 Rails.application.routes.draw do
-  get 'entries/index'
-
-  get 'entries/show'
-
-  get 'entries/new'
-
-  get 'entries/create'
-
-  get 'entries/update'
-
-  get 'entries/edit'
-
-  get 'entries/destroy'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :declarations do
     resources :entries
   end
