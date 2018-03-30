6dcc4bd9f15ddf58507483e59decbdd1f0cfba8c
viktharien <viktharien@zoho.com>
2018-03-29 20:38:05 -0500

    create actions for entries controller

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,18 +1,34 @@
 class EntriesController < ApplicationController
   before_action :set_entry, only: [:show, :update, :destroy]
   def index
+    @entries = Declaration.all
+    json_response(@entries)
   end
 
   def show
+    json_response(@entry.to_json(:include => :entries))
   end
 
   def new
+    @entry = Declaration.new
   end
 
   def create
+    @entry = Phrase.new(params_dec)
+
+    if @entry.save
+      json_response(@entry)
+    else
+      json_response(@entry.errors, :unprocessable_entity)
+    end
   end
 
   def update
+    if @entry.update(params_entry)
+      json_response(@entry)
+    else
+      json_response(@entry.errors, :unprocessable_entity)
+    end
   end
 
   def edit
